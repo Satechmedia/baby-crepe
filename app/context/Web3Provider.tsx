6991e21a26477c685ddddf4fa0603e7c265528cf
Sam Olubunmi <Satechmedia@gmail.com>
2026-01-04 19:49:19 +0100

    fix: add featured wallet IDs to Web3Modal configuration

--- app/context/Web3Provider.tsx
+++ app/context/Web3Provider.tsx
@@ -13,6 +13,14 @@ if (projectId) {
     projectId,
     enableAnalytics: false,
     enableOnramp: false,
+    featuredWalletIds: [
+      '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+      'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+      '0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150', // SafePal
+      '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4', // Binance Web3 Wallet
+      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX
+      '5d9f1395b3a8e848684848dc4147cbd05c8d54bb737eac78fe103901fe6b01a1', // OKX (Alt)
+    ],
     themeMode: 'light',
     themeVariables: {
       '--w3m-accent': '#D4A574',
