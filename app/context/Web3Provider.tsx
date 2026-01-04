ce07e8daf3980a9c1c16799b29c74be970fe2637
Sam Olubunmi <Satechmedia@gmail.com>
2026-01-04 20:07:02 +0100

    fix: add 'ONLY_MOBILE' option to allWallets in Web3Modal configuration

--- app/context/Web3Provider.tsx
+++ app/context/Web3Provider.tsx
@@ -13,6 +13,7 @@ if (projectId) {
     projectId,
     enableAnalytics: false,
     enableOnramp: false,
+    allWallets: 'ONLY_MOBILE',
     featuredWalletIds: [
       '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
       'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
